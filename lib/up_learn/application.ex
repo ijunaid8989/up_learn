1af4188111bf15f99493605fc5264350907ac036
Junaid Farooq <fjunaid252@gmail.com>
2023-04-15 02:48:03 +0500

    Initial Commit.

---
+++ lib/up_learn/application.ex
@@ -0,0 +1,20 @@
+defmodule UpLearn.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: UpLearn.Worker.start_link(arg)
+      # {UpLearn.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: UpLearn.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
---
+++ test/up_learn_test.exs
@@ -0,0 +1,8 @@
+defmodule UpLearnTest do
+  use ExUnit.Case
+  doctest UpLearn
+
+  test "greets the world" do
+    assert UpLearn.hello() == :world
+  end
+end
