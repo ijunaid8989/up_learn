f8c3fca0d89ca3286b6957c3dd1f129455e200c4
Junaid Farooq <fjunaid252@gmail.com>
2023-04-15 11:19:56 +0500

    Added Driver interface.

---
+++ lib/up_learn/scraper/driver.ex
@@ -0,0 +1,44 @@
+defmodule UpLearn.Scraper.Driver do
+  @moduledoc """
+  Main Driver Interface.
+  """
+
+  @typedoc "Scrapper request response"
+  @type response ::
+          {:ok, UpLearn.Scraper.Response.t()}
+          | {:error, {:unexpected_response, UpLearn.Scraper.Response.t()}}
+          | {:error, term}
+
+  @doc false
+  defmacro __using__(opts) do
+    quote location: :keep, bind_quoted: [opts: opts] do
+      @behaviour UpLearn.Scraper.Driver
+
+      import UpLearn.Scraper.Helpers
+
+      @doc false
+      def fetch_in!(data, keys) do
+        get_in(data, keys) || raise(KeyError, key: keys, term: @name)
+      end
+
+      @doc false
+      def dispatch_driver_event(event, meta \\ %{}) do
+        :telemetry.execute(
+          [:driver | event],
+          %{count: 1},
+          meta
+        )
+      end
+
+      @doc false
+      def http_otps(config) do
+        [
+          timeout: 10_000,
+          recv_timeout: 15_000,
+          ssl: [{:versions, [:"tlsv1.2"]}]
+        ]
+        |> Keyword.merge(:maps.to_list(config.ext[:http_options] || %{}))
+      end
+    end
+  end
+end
