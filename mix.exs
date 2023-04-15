67abd94c942268aad0f4e21c8ed4fcce7340cd06
Junaid Farooq <fjunaid252@gmail.com>
2023-04-15 22:45:45 +0500

    Added Deps for mock, bypass, credo.

--- mix.exs
+++ mix.exs
@@ -6,10 +6,14 @@ defmodule UpLearn.MixProject do
       app: :up_learn,
       version: "0.1.0",
       elixir: "~> 1.14",
+      elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
   end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 
   # Run "mix help compile.app" to learn about applications.
   def application do
@@ -25,7 +29,10 @@ defmodule UpLearn.MixProject do
       {:jason, "~> 1.4"},
       {:httpoison, "~> 2.0"},
       {:ecto, "~> 3.8"},
-      {:floki, "~> 0.34.0"}
+      {:floki, "~> 0.34.0"},
+      {:credo, "~> 1.7.0", only: [:dev, :test], runtime: false},
+      {:mock, "~> 0.3.7", only: :test, runtime: false},
+      {:bypass, "~> 2.1", only: :test}
     ]
   end
 end
