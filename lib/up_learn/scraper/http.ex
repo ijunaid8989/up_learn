33cd393925f51f91a44eae095ad56353914e05f2
Junaid Farooq <fjunaid252@gmail.com>
2023-04-15 17:52:32 +0500

    Added HTTP wrapper.

---
+++ lib/up_learn/scraper/http.ex
@@ -0,0 +1,34 @@
+defmodule UpLearn.HTTP do
+  @moduledoc """
+  HTTP Utils.
+
+  we can define the authentication methods here in case the HTTP request need any auth such as basic / digest
+  """
+
+  require Logger
+
+  @type header :: {binary, binary}
+
+  ## API
+
+  @doc false
+  @spec parse_body(binary) :: term
+  def parse_body(json) do
+    case Jason.decode(json) do
+      {:ok, parsed_json} ->
+        parsed_json
+
+      {:error, reason} ->
+        # :ok = LOG.error(message: "error parsing json", error: "#{inspect(reason)}")
+        json
+    end
+  end
+
+  @doc false
+  @spec normalize_headers(HTTPoison.headers()) :: HTTPoison.headers()
+  def normalize_headers(headers) do
+    for {name, value} <- headers do
+      {String.downcase(name), value}
+    end
+  end
+end
